bcab7f8049714cd57051028970cc770a43094337
Shristi1919 <shristis707@gmail.com>
2024-06-02 22:19:28 +0545

    google client detail in config

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+//shristis707
+define('GOOGLE_CLIENT_ID', '486301272571-gk5anqslq34vueqs04u4u3fapthln174.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URI', 'http://localhost/google-calendar/callback.php');
+?>
